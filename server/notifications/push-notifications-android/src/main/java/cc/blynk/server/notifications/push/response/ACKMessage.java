2d170542100683e78897168fe736d9cd5f13bf01
doom369 <doom369@gmail.com>
2015-04-06 19:52:35 +0300

    notification packages movements.

---
+++ server/notifications/push-notifications-android/src/main/java/cc/blynk/server/notifications/push/response/ACKMessage.java
@@ -0,0 +1,14 @@
+package cc.blynk.server.notifications.push.response;
+
+/**
+ * The Blynk Project.
+ * Created by Dmitriy Dumanskiy.
+ * Created on 2/8/2015.
+ */
+public class ACKMessage extends ResponseMessageBase {
+
+    @Override
+    public String toString() {
+        return "ACKMessage{" + super.toString() + "}";
+    }
+}
