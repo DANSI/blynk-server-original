1f6c45c7136efac55f821d1c7504349c4440e4d6
Dmitriy Dumanskiy <doom369@gmail.com>
2015-04-16 23:06:01 +0300

    Embedded certs replaced with netty build-in certs.

--- server/tcp-server/src/main/java/cc/blynk/server/core/application/AppServer.java
+++ server/tcp-server/src/main/java/cc/blynk/server/core/application/AppServer.java
@@ -9,11 +9,11 @@ import cc.blynk.server.handlers.BaseSimpleChannelInboundHandler;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.util.SelfSignedCertificate;
 
 import javax.net.ssl.SSLException;
 import java.io.File;
-import java.net.URISyntaxException;
-import java.net.URL;
+import java.security.cert.CertificateException;
 
 /**
  * The Blynk Project.
@@ -52,17 +52,17 @@ public class AppServer extends BaseServer {
 
     private static SslContext initSslContext(String serverCertPath, String serverKeyPath, String keyPass) {
         try {
+            File serverCert =  new File(serverCertPath);
+            File serverKey = new File(serverKeyPath);
 
-            if ("/server_embedded.crt".equals(serverCertPath) && "/server_embedded.pem".equals(serverKeyPath)) {
-                log.warn("ATTENTION. You using embedded certs. This is not secure. Please replace it with your own certs.");
+            if (!serverCert.exists() || !serverKey.exists()) {
+                log.warn("ATTENTION. Certificate path not valid. Using embedded certs. This is not secure. Please replace it with your own certs.");
+                SelfSignedCertificate ssc = new SelfSignedCertificate();
+                return SslContext.newServerContext(ssc.certificate(), ssc.privateKey());
             }
 
-            File serverCert = resolvePath(serverCertPath);
-            File serverKey = resolvePath(serverKeyPath);
-
-            //todo this is self-signed cerf. just to simplify for now for testing.
             return SslContext.newServerContext(serverCert, serverKey, keyPass);
-        } catch (URISyntaxException e) {
+        } catch (CertificateException e) {
             log.error("Error initializing certs path. Reason : {} ", e.getMessage());
             System.exit(0);
         } catch (SSLException e) {
@@ -74,19 +74,6 @@ public class AppServer extends BaseServer {
             System.exit(0);
         }
         return null;
-    }
-
-    private static File resolvePath(String pathToFile) throws URISyntaxException {
-        File file = new File(pathToFile);
-        if (!file.exists()) {
-            URL url = AppServer.class.getResource(pathToFile);
-            if (url == null) {
-                throw new IllegalStateException(pathToFile + " - not available.");
-            }
-            file = new File(url.toURI());
-        }
-
-        return file;
     }
 
     @Override
