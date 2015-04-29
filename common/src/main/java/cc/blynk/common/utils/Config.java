61aa937e73028d77d6753100f344d5a9623eaaba
Dmitriy Dumanskiy <doom369@gmail.com>
2015-04-30 01:33:17 +0300

    Explicit UTF-8 encoding

--- common/src/main/java/cc/blynk/common/utils/Config.java
+++ common/src/main/java/cc/blynk/common/utils/Config.java
@@ -9,7 +9,7 @@ import java.nio.charset.Charset;
  */
 public class Config {
 
-    public static final Charset DEFAULT_CHARSET = Charset.defaultCharset();
+    public static final Charset DEFAULT_CHARSET = Charset.forName("UTF-8");
 
     public static final String SERVER_PROPERTIES_FILENAME = "server.properties";
     public static final String MAIL_PROPERTIES_FILENAME = "mail.properties";
