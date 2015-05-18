e4fbf9d4fd8d2324042649439b7c9bc2be279007
Dmitriy Dumanskiy <doom369@gmail.com>
2015-05-19 00:28:57 +0300

    additional reset pass tool

--- server/administration/src/main/java/cc/blynk/server/core/administration/actions/ResetPassword.java
+++ server/administration/src/main/java/cc/blynk/server/core/administration/actions/ResetPassword.java
@@ -1,6 +1,5 @@
 package cc.blynk.server.core.administration.actions;
 
-import cc.blynk.common.administration.SHA256Util;
 import cc.blynk.server.core.administration.Executable;
 import cc.blynk.server.dao.SessionsHolder;
 import cc.blynk.server.dao.UserRegistry;
@@ -12,15 +11,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Utility class for manual user pass changing. Expects plain password, not hashed.
+ * Utility class for auto user pass changing. Expected hashed password
  *
  * The Blynk Project.
  * Created by Dmitriy Dumanskiy.
  * Created on 21.04.15.
  */
-public class ManualResetPassword implements Executable {
+public class ResetPassword implements Executable {
 
-    private static final Logger log = LogManager.getLogger(ManualResetPassword.class);
+    private static final Logger log = LogManager.getLogger(ResetPassword.class);
 
     @Override
     public List<String> execute(UserRegistry userRegistry, SessionsHolder sessionsHolder, String... params) {
@@ -33,7 +32,7 @@ public class ManualResetPassword implements Executable {
             if (user == null) {
                 log.error("User '{}' not exists.", userName);
             } else {
-                user.setPass(SHA256Util.makeHash(newPass, userName));
+                user.setPass(newPass);
                 user.setLastModifiedTs(System.currentTimeMillis());
                 log.info("Password updated");
                 result.add("Password updated.\n");
