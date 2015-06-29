42017f00120ff3e5ed20c3dcbce699562f9a2f07
Dmitriy Dumanskiy <doom369@gmail.com>
2015-06-29 11:28:36 +0300

    Fixing tests

--- server/tcp-server/src/test/java/cc/blynk/server/workers/notifications/NotificationsProcessorTest.java
+++ server/tcp-server/src/test/java/cc/blynk/server/workers/notifications/NotificationsProcessorTest.java
@@ -1,7 +1,11 @@
 package cc.blynk.server.workers.notifications;
 
+import cc.blynk.server.model.auth.ChannelState;
+import cc.blynk.server.model.auth.User;
 import io.netty.channel.Channel;
 import io.netty.channel.EventLoop;
+import io.netty.util.Attribute;
+import io.netty.util.AttributeKey;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -27,6 +31,47 @@ public class NotificationsProcessorTest {
     @Test
     public void testNoCorrectWrapper() {
         when(channel.eventLoop()).thenReturn(eventLoop);
+        when(channel.attr(eq(ChannelState.USER))).thenReturn(new Attribute<User>() {
+            @Override
+            public AttributeKey<User> key() {
+                return null;
+            }
+
+            @Override
+            public User get() {
+                return null;
+            }
+
+            @Override
+            public void set(User user) {
+
+            }
+
+            @Override
+            public User getAndSet(User user) {
+                return null;
+            }
+
+            @Override
+            public User setIfAbsent(User user) {
+                return null;
+            }
+
+            @Override
+            public User getAndRemove() {
+                return null;
+            }
+
+            @Override
+            public boolean compareAndSet(User user, User t1) {
+                return false;
+            }
+
+            @Override
+            public void remove() {
+
+            }
+        });
         NotificationsProcessor processor = new NotificationsProcessor(5);
         processor.twit(channel, "token", "secret", "body", 1);
         verify(channel, timeout(2000)).eventLoop();
