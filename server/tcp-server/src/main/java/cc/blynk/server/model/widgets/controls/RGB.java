b114cedffa24a3f1339b24c8d561fc8623f9e5c9
Dmitriy Dumanskiy <doom369@gmail.com>
2015-07-03 13:32:50 +0300

    More fields for RGB

--- server/tcp-server/src/main/java/cc/blynk/server/model/widgets/controls/RGB.java
+++ server/tcp-server/src/main/java/cc/blynk/server/model/widgets/controls/RGB.java
@@ -1,5 +1,6 @@
 package cc.blynk.server.model.widgets.controls;
 
+import cc.blynk.server.model.enums.PinType;
 import cc.blynk.server.model.widgets.Widget;
 
 /**
@@ -15,9 +16,13 @@ public class RGB extends Widget {
 
     public boolean pwmMode2;
 
+    public PinType pinType2;
+
     public Byte pin3;
 
     public boolean pwmMode3;
+
+    public PinType pinType3;
 
     public boolean splitMode;
 
