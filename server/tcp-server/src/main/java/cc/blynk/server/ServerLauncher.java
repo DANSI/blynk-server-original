36ba7be20eff41db6fbaa5e67b71d393c920ede0
Dmitriy Dumanskiy <doom369@gmail.com>
2015-08-17 12:44:23 +0300

    less memory consumption for default servers.

--- server/tcp-server/src/main/java/cc/blynk/server/ServerLauncher.java
+++ server/tcp-server/src/main/java/cc/blynk/server/ServerLauncher.java
@@ -86,6 +86,8 @@ public class ServerLauncher {
 
         //required to make all loggers async with LMAX disruptor
         System.setProperty("Log4jContextSelector", "org.apache.logging.log4j.core.async.AsyncLoggerContextSelector");
+        System.setProperty("AsyncLogger.RingBufferSize",
+                serverProperties.getProperty("async.logger.ring.buffer.size", String.valueOf(8 * 1024)));
 
         //configurable folder for logs via property.
         System.setProperty("logs.folder", serverProperties.getProperty("logs.folder"));
