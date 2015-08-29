14a93f9e681d6bb7596f81f2eca24b81d718b488
Dmitriy Dumanskiy <doom369@gmail.com>
2015-08-29 20:34:48 +0300

    One more test.

--- server/tcp-server/src/test/java/cc/blynk/server/storage/reporting/average/AverageAggregatorTest.java
+++ server/tcp-server/src/test/java/cc/blynk/server/storage/reporting/average/AverageAggregatorTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.time.ZonedDateTime;
+import java.time.ZoneOffset;
 
 import static cc.blynk.server.storage.reporting.average.AverageAggregator.DAY;
 import static cc.blynk.server.storage.reporting.average.AverageAggregator.HOURS;
@@ -20,18 +20,20 @@ public class AverageAggregatorTest {
 
     private static ZoneId zoneId = ZoneId.of("America/Los_Angeles");
 
+    private static long getMillis(int year, int month, int dayOfMonth, int hour, int minute) {
+        LocalDateTime dateTime = LocalDateTime.of(year, month, dayOfMonth, hour, minute);
+        return dateTime.toInstant(ZoneOffset.ofTotalSeconds(0)).toEpochMilli();
+    }
+
     @Test
     public void testAverageWorksOkForOnePin() {
-        LocalDateTime dateTime = LocalDateTime.of(2015, 8, 1, 1, 0);
-        ZonedDateTime zdt = dateTime.atZone(zoneId);
-        long ts = zdt.toInstant().toEpochMilli();
-
-
         AverageAggregator averageAggregator = new AverageAggregator();
         String username = "test";
         PinType pinType = PinType.VIRTUAL;
         int dashId = 1;
         byte pin = 1;
+
+        long ts = getMillis(2015, 8, 1, 0, 0);
 
         int COUNT = 100;
 
@@ -47,7 +49,40 @@ public class AverageAggregatorTest {
 
         assertEquals(expectedAverage, averageAggregator.getHourly().get(new AggregationKey(username, dashId, pinType, pin, ts / HOURS)).calcAverage(), 0);
         assertEquals(expectedAverage, averageAggregator.getDaily().get(new AggregationKey(username, dashId, pinType, pin, ts / DAY)).calcAverage(), 0);
+    }
 
+    @Test
+    public void testAverageWorksForOneDay() {
+        AverageAggregator averageAggregator = new AverageAggregator();
+        String username = "test";
+        PinType pinType = PinType.VIRTUAL;
+        int dashId = 1;
+        byte pin = 1;
+
+        double expectedDailyAverage = 0;
+
+        int COUNT = 100;
+
+        for (int hour = 0; hour < 24; hour++) {
+            long ts = getMillis(2015, 8, 1, hour, 0);
+
+            double expectedAverage = 0;
+            for (int i = 0; i < COUNT; i++) {
+                expectedAverage += i;
+                averageAggregator.collect(username, dashId, pinType, pin, ts, String.valueOf(i));
+            }
+            expectedDailyAverage += expectedAverage;
+            expectedAverage /= COUNT;
+
+            assertEquals(hour + 1, averageAggregator.getHourly().size());
+
+            assertEquals(expectedAverage, averageAggregator.getHourly().get(new AggregationKey(username, dashId, pinType, pin, ts / HOURS)).calcAverage(), 0);
+        }
+        expectedDailyAverage /= COUNT * 24;
+
+        assertEquals(24, averageAggregator.getHourly().size());
+        assertEquals(1, averageAggregator.getDaily().size());
+        assertEquals(expectedDailyAverage, averageAggregator.getDaily().get(new AggregationKey(username, dashId, pinType, pin, getMillis(2015, 8, 1, 0, 0) / DAY)).calcAverage(), 0);
     }
 
 }
