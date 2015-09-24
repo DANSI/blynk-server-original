44cfe9d98db731596fbdfe56bd118b9c21cd25f6
Dmitriy Dumanskiy <doom369@gmail.com>
2015-09-24 13:30:55 +0300

    One more test

--- integration-tests/src/test/java/cc/blynk/integration/ShareProfileWorkflowTest.java
+++ integration-tests/src/test/java/cc/blynk/integration/ShareProfileWorkflowTest.java
@@ -20,11 +20,12 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.List;
 
+import static cc.blynk.common.enums.Response.INVALID_TOKEN;
 import static cc.blynk.common.enums.Response.NOT_ALLOWED;
 import static cc.blynk.common.model.messages.MessageFactory.produce;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
+
 
 /**
  * The Blynk Project.
@@ -98,7 +99,48 @@ public class ShareProfileWorkflowTest extends IntegrationBase {
         Notification notification = profile.dashBoards[0].getWidgetByType(Notification.class);
         notification.cleanPrivateData();
         assertEquals(profile.dashBoards[0].toString(), dashboard);
-        System.out.println(dashboard);
+        //System.out.println(dashboard);
     }
+
+    @Test
+    public void testGetShareTokenAndRefresh() throws Exception {
+        clientPair.appClient.send("getShareToken 1");
+
+        String token = getBody(clientPair.appClient.responseMock);
+        assertNotNull(token);
+        assertEquals(32, token.length());
+
+        ClientPair clientPair2 = initAppAndHardPair("localhost", appPort, hardPort, "dima2@mail.ua 1", "user_profile_json_2.txt", properties);
+        clientPair2.appClient.send("getSharedDash " + token);
+
+        String dashboard = getBody(clientPair2.appClient.responseMock);
+
+        assertNotNull(dashboard);
+
+        clientPair.appClient.reset();
+        clientPair.appClient.send("refreshShareToken 1");
+        String refreshedToken = getBody(clientPair.appClient.responseMock);
+        assertNotNull(refreshedToken);
+        assertNotEquals(refreshedToken, token);
+
+        clientPair.appClient.reset();
+        clientPair.appClient.send("getSharedDash " + token);
+        verify(clientPair.appClient.responseMock, timeout(500)).channelRead(any(), eq(produce(1, INVALID_TOKEN)));
+
+        clientPair.appClient.reset();
+        clientPair.appClient.send("getSharedDash " + refreshedToken);
+
+        dashboard = getBody(clientPair.appClient.responseMock);
+
+        assertNotNull(dashboard);
+        Profile profile = JsonParser.parseProfile(readTestUserProfile(), 1);
+        Twitter twitter = profile.dashBoards[0].getWidgetByType(Twitter.class);
+        twitter.cleanPrivateData();
+        Notification notification = profile.dashBoards[0].getWidgetByType(Notification.class);
+        notification.cleanPrivateData();
+        assertEquals(profile.dashBoards[0].toString(), dashboard);
+        //System.out.println(dashboard);
+    }
+
 
 }
