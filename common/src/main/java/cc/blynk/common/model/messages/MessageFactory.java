d857c1f4dee5b78aeaecb9cd1e53d0fb11cc68e8
Dmitriy Dumanskiy <doom369@gmail.com>
2015-09-29 15:44:43 +0300

    switch reoder

--- common/src/main/java/cc/blynk/common/model/messages/MessageFactory.java
+++ common/src/main/java/cc/blynk/common/model/messages/MessageFactory.java
@@ -22,36 +22,36 @@ public class MessageFactory {
 
     public static Message produce(int messageId, short command, String body) {
         switch (command) {
-            case HARDWARE:
-                return new HardwareMessage(messageId, body);
-            case PING :
-                return new PingMessage(messageId, body);
+            case REGISTER :
+                return new RegisterMessage(messageId, body);
+            case LOGIN :
+                return new LoginMessage(messageId, body);
             case SAVE_PROFILE :
                 return new SaveProfileMessage(messageId, body);
             case LOAD_PROFILE :
                 return new LoadProfileMessage(messageId, body);
+            case GET_TOKEN :
+                return new GetTokenMessage(messageId, body);
+            case PING :
+                return new PingMessage(messageId, body);
             case ACTIVATE_DASHBOARD :
                 return new ActivateDashboardMessage(messageId, body);
             case DEACTIVATE_DASHBOARD :
                 return new DeActivateDashboardMessage(messageId, body);
-            case GET_TOKEN :
-                return new GetTokenMessage(messageId, body);
             case REFRESH_TOKEN :
                 return new RefreshTokenMessage(messageId, body);
             case GET_GRAPH_DATA :
                 return new GetGraphDataMessage(messageId, body);
-            case LOGIN :
-                return new LoginMessage(messageId, body);
             case TWEET :
                 return new TweetMessage(messageId, body);
             case EMAIL :
                 return new MailMessage(messageId, body);
             case PUSH_NOTIFICATION :
                 return new PushMessage(messageId, body);
-            case REGISTER :
-                return new RegisterMessage(messageId, body);
             case BRIDGE :
                 return new BridgeMessage(messageId, body);
+            case HARDWARE:
+                return new HardwareMessage(messageId, body);
 
             default: throw new UnsupportedCommandException(String.format("Command with code %d not supported message.", command), messageId);
         }
