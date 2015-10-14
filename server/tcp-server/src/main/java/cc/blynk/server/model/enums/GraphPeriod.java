a62e8d6df85e40d809398b6fa0dd9ffe41d18196
Dmitriy Dumanskiy <doom369@gmail.com>
2015-10-14 15:39:14 +0300

    #29 fix enum

--- server/tcp-server/src/main/java/cc/blynk/server/model/enums/GraphPeriod.java
+++ server/tcp-server/src/main/java/cc/blynk/server/model/enums/GraphPeriod.java
@@ -7,25 +7,12 @@ package cc.blynk.server.model.enums;
  */
 public enum GraphPeriod {
 
-    DAY(24),
-    WEEK(24 * 7),
-    MONTH(30 * 24),
-    THREE_MONTHS(3 * 30 * 24),
-    ALL(100 * 12 * 30 * 24);
-
-    public int periodInHours;
-
-    GraphPeriod(int hours) {
-        this.periodInHours = hours;
-    }
-
-    public static GraphPeriod getPeriodByHoursCount(int period) {
-        for (GraphPeriod graphPeriod : values()) {
-            if (period == graphPeriod.periodInHours) {
-                return graphPeriod;
-            }
-        }
-        return null;
-    }
+    ONE_HOUR,
+    SIX_HOURS,
+    DAY,
+    WEEK,
+    MONTH,
+    THREE_MONTHS,
+    ALL;
 
 }
