bc192c7427ef43bf350d39647c0d320e72abac0e
Dmitriy Dumanskiy <doom369@gmail.com>
2015-10-16 22:09:43 +0300

    more logging.

--- server/tcp-server/src/main/java/cc/blynk/server/handlers/app/logic/DeActivateDashboardLogic.java
+++ server/tcp-server/src/main/java/cc/blynk/server/handlers/app/logic/DeActivateDashboardLogic.java
@@ -5,6 +5,8 @@ import cc.blynk.common.utils.ParseUtil;
 import cc.blynk.server.model.DashBoard;
 import cc.blynk.server.model.auth.User;
 import io.netty.channel.ChannelHandlerContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import static cc.blynk.common.enums.Response.OK;
 import static cc.blynk.common.model.messages.MessageFactory.produce;
@@ -17,8 +19,11 @@ import static cc.blynk.common.model.messages.MessageFactory.produce;
  */
 public class DeActivateDashboardLogic {
 
+    private static final Logger log = LogManager.getLogger(ActivateDashboardLogic.class);
+
     public static void messageReceived(ChannelHandlerContext ctx, User user, Message message) {
         if (message.length > 0) {
+            log.debug("Activating dash {} for user {}", message.body, user.name);
             int dashId = ParseUtil.parseInt(message.body, message.id);
             DashBoard dashBoard = user.profile.getDashboardById(dashId, message.id);
             dashBoard.isActive = false;
