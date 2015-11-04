af5034ae67b6b07df3bbf0133d91cc0c51a8702e
Dmitriy Dumanskiy <doom369@gmail.com>
2015-11-04 02:43:04 +0200

    fix for lcd

--- server/tcp-server/src/main/java/cc/blynk/server/model/widgets/outputs/LCD.java
+++ server/tcp-server/src/main/java/cc/blynk/server/model/widgets/outputs/LCD.java
@@ -1,11 +1,14 @@
 package cc.blynk.server.model.widgets.outputs;
 
+import cc.blynk.server.model.HardwareBody;
+import cc.blynk.server.model.Pin;
+
 /**
  * The Blynk Project.
  * Created by Dmitriy Dumanskiy.
  * Created on 21.03.15.
  */
-public class LCD extends FrequencyMultiPinWidget {
+public class LCD extends FrequencyWidget {
 
     public boolean advancedMode;
 
@@ -14,4 +17,27 @@ public class LCD extends FrequencyMultiPinWidget {
     public String textFormatLine2;
 
     public boolean textLight;
+
+    public Pin[] pins;
+
+    @Override
+    public void updateIfSame(HardwareBody body) {
+        if (pins != null) {
+            for (int i = 0; i < pins.length; i++) {
+                if (pins[i].isSame(body)) {
+                    pins[i].value = (body.value.length > 1 ? body.value[i] : body.value[0]);
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean isSame(HardwareBody body) {
+        for (Pin pin : pins) {
+            if (pin.isSame(body)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
