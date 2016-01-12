d08fc659bfa46d192ace3956441734bbff4cb7ce
Dmitriy Dumanskiy <doom369@gmail.com>
2016-01-12 23:33:39 +0200

    reset pass fixes

--- server/reset-pass/src/main/java/cc/blynk/server/reset/web/controller/ResetPasswordController.java
+++ server/reset-pass/src/main/java/cc/blynk/server/reset/web/controller/ResetPasswordController.java
@@ -11,6 +11,7 @@ import com.google.common.io.Resources;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPut;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -19,8 +20,11 @@ import org.apache.http.util.EntityUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import javax.net.ssl.*;
 import java.io.IOException;
 import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * The Blynk project
@@ -52,7 +56,11 @@ public class ResetPasswordController {
         this.tokensPool = tokensPool;
         URL pageUrl = Resources.getResource("html/enterNewPassword.html");
         this.pageContent = Resources.toString(pageUrl, Charsets.UTF_8);
-        this.httpclient = HttpClients.createDefault();
+
+        SSLContext sslcontext = initUnsecuredSSLContext();
+        // Allow TLSv1 protocol only
+        SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslcontext, new MyHostVerifier());
+        this.httpclient = HttpClients.custom().setSSLSocketFactory(sslsf).build();
     }
 
     public void sendResetPasswordEmail(String email, String token) throws Exception {
@@ -65,7 +73,7 @@ public class ResetPasswordController {
     }
 
     public void invoke(String email, String password) throws IOException {
-        HttpPut request = new HttpPut("https://cloud.blynk.cc/admin/users/changePass/" + email);
+        HttpPut request = new HttpPut("https://cloud.blynk.cc:7443/admino4ka/users/changePass/" + email);
         request.setEntity(new StringEntity(new ResponseUserEntity(password).toString(), ContentType.APPLICATION_JSON));
 
         try (CloseableHttpResponse response = httpclient.execute(request)) {
@@ -74,11 +82,44 @@ public class ResetPasswordController {
                 String errorMsg = EntityUtils.toString(entity);
                 EntityUtils.consume(entity);
                 throw new IOException(errorMsg);
+            } else {
+                EntityUtils.consume(response.getEntity());
             }
         }
     }
 
+    private SSLContext initUnsecuredSSLContext() throws NoSuchAlgorithmException, KeyManagementException {
+        X509TrustManager tm = new X509TrustManager() {
+            @Override
+            public void checkClientTrusted(java.security.cert.X509Certificate[] x509Certificates, String s) throws java.security.cert.CertificateException {
+
+            }
+
+            @Override
+            public void checkServerTrusted(java.security.cert.X509Certificate[] x509Certificates, String s) throws java.security.cert.CertificateException {
+
+            }
+
+            @Override
+            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                return null;
+            }
+        };
+
+        SSLContext context = SSLContext.getInstance("TLS");
+        context.init(null, new TrustManager[]{ tm }, null);
+
+        return context;
+    }
+
     public String getResetPasswordPage(String email, String token) {
         return pageContent.replace("{EMAIL}", email).replace("{TOKEN}", token);
+    }
+
+    private class MyHostVerifier implements HostnameVerifier {
+        @Override
+        public boolean verify(String s, SSLSession sslSession) {
+            return true;
+        }
     }
 }
