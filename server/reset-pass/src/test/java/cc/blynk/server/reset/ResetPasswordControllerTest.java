c897919e676741501cbead031b469270f541e384
Dmitriy Dumanskiy <doom369@gmail.com>
2016-01-15 01:46:59 +0200

    test fix

--- server/reset-pass/src/test/java/cc/blynk/server/reset/ResetPasswordControllerTest.java
+++ server/reset-pass/src/test/java/cc/blynk/server/reset/ResetPasswordControllerTest.java
@@ -1,26 +1,20 @@
 package cc.blynk.server.reset;
 
-import cc.blynk.server.notifications.mail.MailWrapper;
 import cc.blynk.server.reset.web.controller.ResetPasswordController;
 import cc.blynk.server.reset.web.entities.TokensPool;
+import cc.blynk.utils.ServerProperties;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ResetPasswordControllerTest {
 
-    @Mock
-    private MailWrapper mailWrapper;
-    @Mock
-    private Runnable runnable;
-
     @Test
     public void testGetResetPasswordPage() throws Exception {
-        ResetPasswordController controller = new ResetPasswordController("http://localhost", 8080, new TokensPool(), mailWrapper);
+        ResetPasswordController controller = new ResetPasswordController("http://localhost", 8080, new TokensPool(), new ServerProperties());
         String resetPasswordPage = controller.getResetPasswordPage("", "");
         assertNotNull(resetPasswordPage);
     }
