b4c18f69ebe00994a315baa0bbdfdbfddd228f5e
Dmitriy Dumanskiy <doom369@gmail.com>
2016-01-26 19:13:20 +0200

    #100 support for CIDR notation for allowed IPs

--- server/http-admin/src/main/java/cc/blynk/server/admin/http/handlers/IpFilterHandler.java
+++ server/http-admin/src/main/java/cc/blynk/server/admin/http/handlers/IpFilterHandler.java
@@ -1,10 +1,14 @@
 package cc.blynk.server.admin.http.handlers;
 
+import cc.blynk.utils.ParseUtil;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.ipfilter.AbstractRemoteAddressFilter;
+import io.netty.handler.ipfilter.IpFilterRuleType;
+import io.netty.handler.ipfilter.IpSubnetFilterRule;
 
 import java.net.InetSocketAddress;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -15,14 +19,38 @@ import java.util.Set;
 @ChannelHandler.Sharable
 public class IpFilterHandler extends AbstractRemoteAddressFilter<InetSocketAddress> {
 
-    private final Set<String> allowedIPs;
+    private final Set<String> allowedIPs = new HashSet<>();
+    private final Set<IpSubnetFilterRule> rules = new HashSet<>();
 
     public IpFilterHandler(Set<String> allowedIPs) {
-        this.allowedIPs = allowedIPs;
+        for (String allowedIP : allowedIPs) {
+            if (allowedIP.contains("/")) {
+                String[] split = allowedIP.split("/");
+                String ip = split[0];
+                int cidr = ParseUtil.parseInt(split[1]);
+                this.rules.add(new IpSubnetFilterRule(ip, cidr, IpFilterRuleType.ACCEPT));
+            } else {
+                this.allowedIPs.add(allowedIP);
+            }
+        }
     }
 
     @Override
     protected boolean accept(ChannelHandlerContext ctx, InetSocketAddress remoteAddress) throws Exception {
-        return !(allowedIPs != null && !allowedIPs.contains(remoteAddress.getAddress().getHostAddress()));
+        if (allowedIPs.size() == 0 && rules.size() == 0) {
+            return false;
+        }
+
+        if (allowedIPs.contains(remoteAddress.getAddress().getHostAddress())) {
+            return true;
+        }
+
+        for (IpSubnetFilterRule rule : rules) {
+            if (rule.matches(remoteAddress)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
