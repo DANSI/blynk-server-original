9c5406639fd0e2871789035da15313f3bca5e2e5
Dmitriy Dumanskiy <doom369@gmail.com>
2016-01-26 20:57:22 +0200

    Revert led changes.

--- server/core/src/main/java/cc/blynk/server/core/model/widgets/outputs/LED.java
+++ server/core/src/main/java/cc/blynk/server/core/model/widgets/outputs/LED.java
@@ -7,13 +7,29 @@ import cc.blynk.server.core.model.widgets.OnePinWidget;
  * Created by Dmitriy Dumanskiy.
  * Created on 21.03.15.
  */
-public class LED extends OnePinWidget {
+//todo remove FrequencyWidget from LED when users will migrate to new clients.
+public class LED extends OnePinWidget implements FrequencyWidget {
 
     public int color;
 
     private int frequency;
 
     private transient long lastRequestTS;
+
+    @Override
+    public final int getFrequency() {
+        return frequency;
+    }
+
+    @Override
+    public final long getLastRequestTS(String body) {
+        return lastRequestTS;
+    }
+
+    @Override
+    public final void setLastRequestTS(String body, long now) {
+        this.lastRequestTS = now;
+    }
 
     @Override
     public String getModeType() {
