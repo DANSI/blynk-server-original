d9751c5bd5d376255c8189b0c3be6ddc609aa393
Dmitriy Dumanskiy <doom369@gmail.com>
2016-02-14 22:33:45 +0200

    Cleanup

--- server/db/src/test/java/cc/blynk/server/db/CassandraTestBase.java
+++ server/db/src/test/java/cc/blynk/server/db/CassandraTestBase.java
@@ -1,7 +1,6 @@
 package cc.blynk.server.db;
 
 import cc.blynk.server.core.model.enums.PinType;
-import com.datastax.driver.core.BatchStatement;
 import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Row;
 import org.junit.AfterClass;
@@ -19,7 +18,7 @@ public class CassandraTestBase {
 
     @BeforeClass
     public static void init() {
-        client = new SimpleClient("127.0.0.1");
+        client = new SimpleClient("mykeyspace", "127.0.0.1");
     }
 
     @AfterClass
@@ -29,19 +28,19 @@ public class CassandraTestBase {
 
     @Test
     public void testInsertSomeData() {
-        BatchStatement batch = new BatchStatement();
-        batch.add(client.insertIntoReportingMinute.bind("test@gmail.com", 1, 0, Character.toString(PinType.VIRTUAL.pintTypeChar), System.currentTimeMillis(), 0.1));
-        batch.add(client.insertIntoReportingMinute.bind("test@gmail.com", 1, 0, Character.toString(PinType.VIRTUAL.pintTypeChar), System.currentTimeMillis() + 1, 0.2));
-        batch.add(client.insertIntoReportingMinute.bind("test@gmail.com", 1, 0, Character.toString(PinType.VIRTUAL.pintTypeChar), System.currentTimeMillis() + 2, 0.3));
-        batch.add(client.insertIntoReportingMinute.bind("test@gmail.com", 1, 0, Character.toString(PinType.VIRTUAL.pintTypeChar), System.currentTimeMillis() + 3, 0.4));
-        batch.add(client.insertIntoReportingMinute.bind("test@gmail.com", 1, 0, Character.toString(PinType.VIRTUAL.pintTypeChar), System.currentTimeMillis() - 1000, 0.0));
-        batch.add(client.insertIntoReportingMinute.bind("test@gmail.com", 1, 1, Character.toString(PinType.VIRTUAL.pintTypeChar), System.currentTimeMillis(), 1.1));
-        client.session.execute(batch);
+        client.batch(
+            client.insertIntoReportingMinute.bind("test@gmail.com", 1, 0, Character.toString(PinType.VIRTUAL.pintTypeChar), System.currentTimeMillis(), 0.1),
+            client.insertIntoReportingMinute.bind("test@gmail.com", 1, 0, Character.toString(PinType.VIRTUAL.pintTypeChar), System.currentTimeMillis() + 1, 0.2),
+            client.insertIntoReportingMinute.bind("test@gmail.com", 1, 0, Character.toString(PinType.VIRTUAL.pintTypeChar), System.currentTimeMillis() + 2, 0.3),
+            client.insertIntoReportingMinute.bind("test@gmail.com", 1, 0, Character.toString(PinType.VIRTUAL.pintTypeChar), System.currentTimeMillis() + 3, 0.4),
+            client.insertIntoReportingMinute.bind("test@gmail.com", 1, 0, Character.toString(PinType.VIRTUAL.pintTypeChar), System.currentTimeMillis() - 1000, 0.0),
+            client.insertIntoReportingMinute.bind("test@gmail.com", 1, 1, Character.toString(PinType.VIRTUAL.pintTypeChar), System.currentTimeMillis(), 1.1)
+        );
     }
 
     @Test
     public void selectSomeData() {
-        ResultSet results = client.session.execute(client.selectFromReportingMinute.bind("test@gmail.com", 1, 0, Character.toString(PinType.VIRTUAL.pintTypeChar)));
+        ResultSet results = client.session.execute(client.selectFromReportingMinute.bind("test@gmail.com", 1, 0, Character.toString(PinType.VIRTUAL.pintTypeChar), 10));
 
         //Iterable i = results.iterator();
 
