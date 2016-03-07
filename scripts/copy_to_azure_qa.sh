f20f88fc467b01e935e935942197ec3e876dc8a2
Dmitriy Dumanskiy <doom369@gmail.com>
2016-03-07 23:15:56 +0200

    Readme update

--- scripts/copy_to_azure_qa.sh
+++ scripts/copy_to_azure_qa.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-scp ../server/launcher/target/server-0.13.2-SNAPSHOT.jar azureuser@blynk-qa.cloudapp.net:/home/azureuser
+scp ../server/launcher/target/server-0.13.3-SNAPSHOT.jar azureuser@blynk-qa.cloudapp.net:/home/azureuser
