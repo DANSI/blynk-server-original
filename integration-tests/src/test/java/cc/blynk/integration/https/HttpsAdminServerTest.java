e0cd426926369b5dba4606e376d82ec0434f4c9d
Dmitriy Dumanskiy <doom369@gmail.com>
2016-03-13 01:19:17 +0200

    removed outdated tests

--- integration-tests/src/test/java/cc/blynk/integration/https/HttpsAdminServerTest.java
+++ integration-tests/src/test/java/cc/blynk/integration/https/HttpsAdminServerTest.java
@@ -12,7 +12,6 @@ import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -92,43 +91,6 @@ public class HttpsAdminServerTest extends BaseTest {
 
         try (CloseableHttpResponse response = httpclient.execute(request)) {
             assertEquals(404, response.getStatusLine().getStatusCode() );
-        }
-    }
-
-    @Test
-    public void testServerResetPass() throws Exception {
-        for (int i = 0; i < 100; i++) {
-            String name = "dima@dima.ua";
-            HttpPut request = new HttpPut(httpsServerUrl + "changePass/" + name);
-            request.setEntity(new StringEntity(new ResponseUserEntity("pass").toString(), ContentType.APPLICATION_JSON));
-
-            try (CloseableHttpResponse response = httpclient.execute(request)) {
-                EntityUtils.consume(response.getEntity());
-                assertEquals(404, response.getStatusLine().getStatusCode());
-            }
-        }
-
-        for (int i = 0; i < 100; i++) {
-            String name = "dmitriy@blynk.cc";
-            HttpPut request = new HttpPut(httpsServerUrl + "changePass/" + name);
-            request.setEntity(new StringEntity(new ResponseUserEntity("pass").toString(), ContentType.APPLICATION_JSON));
-
-            try (CloseableHttpResponse response = httpclient.execute(request)) {
-                EntityUtils.consume(response.getEntity());
-                assertEquals(200, response.getStatusLine().getStatusCode());
-            }
-        }
-    }
-
-    @Test
-    public void testChangePassCorrect() throws Exception {
-        String testUser = "dmitriy@blynk.cc";
-        HttpPut request = new HttpPut(httpsServerUrl + "changePass/" + testUser);
-        request.setEntity(new StringEntity(new ResponseUserEntity("123").toString(), ContentType.APPLICATION_JSON));
-
-        try (CloseableHttpResponse response = httpclient.execute(request)) {
-            assertEquals(200, response.getStatusLine().getStatusCode());
-            EntityUtils.consume(response.getEntity());
         }
     }
 
--- scripts/copy_to_azure_qa.sh
+++ scripts/copy_to_azure_qa.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-scp ../server/launcher/target/server-0.13.3-SNAPSHOT.jar azureuser@blynk-qa.cloudapp.net:/home/azureuser
+scp ../server/launcher/target/server-0.13.4-SNAPSHOT.jar azureuser@blynk-qa.cloudapp.net:/home/azureuser
