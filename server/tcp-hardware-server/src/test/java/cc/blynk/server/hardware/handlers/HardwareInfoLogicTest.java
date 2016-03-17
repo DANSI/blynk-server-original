78a18b4449469b12b161b04b7a2545ab652f00e9
Dmitriy Dumanskiy <doom369@gmail.com>
2016-03-17 22:04:15 +0200

    one more test fix

--- server/tcp-hardware-server/src/test/java/cc/blynk/server/hardware/handlers/HardwareInfoLogicTest.java
+++ server/tcp-hardware-server/src/test/java/cc/blynk/server/hardware/handlers/HardwareInfoLogicTest.java
@@ -1,9 +1,10 @@
 package cc.blynk.server.hardware.handlers;
 
-import cc.blynk.server.core.protocol.model.messages.ResponseMessage;
 import cc.blynk.server.core.protocol.model.messages.hardware.HardwareInfoMessage;
 import cc.blynk.server.hardware.handlers.hardware.logic.HardwareInfoLogic;
 import cc.blynk.utils.ServerProperties;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelPipeline;
 import io.netty.handler.timeout.ReadTimeoutHandler;
@@ -12,7 +13,6 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import static cc.blynk.server.core.protocol.enums.Response.*;
 import static org.mockito.Mockito.*;
 
 /**
@@ -31,16 +31,31 @@ public class HardwareInfoLogicTest {
     @Mock
     private ChannelPipeline pipeline;
 
+    @Mock
+    private ByteBufAllocator allocator;
+
+    @Mock
+    private ByteBuf byteBuf;
+
+
+
     @Test
     public void testCorrectBehavior() {
         HardwareInfoLogic logic = new HardwareInfoLogic(props.getIntProperty("hard.socket.idle.timeout", 0));
+
         when(ctx.pipeline()).thenReturn(pipeline);
+        when(ctx.alloc()).thenReturn(allocator);
+        when(allocator.ioBuffer(anyInt())).thenReturn(byteBuf);
+        when(byteBuf.writeByte(anyInt())).thenReturn(byteBuf);
+        when(byteBuf.writeShort(anyShort())).thenReturn(byteBuf);
+        when(byteBuf.writeShort(anyShort())).thenReturn(byteBuf);
+
         HardwareInfoMessage hardwareInfoLogic = new HardwareInfoMessage(1, "ver 0.3.2-beta h-beat 60 buff-in 256 dev ESP8266".replaceAll(" ", "\0"));
         logic.messageReceived(ctx, null, hardwareInfoLogic);
 
         verify(pipeline).remove(ReadTimeoutHandler.class);
         verify(pipeline).addFirst(any(ReadTimeoutHandler.class));
-        verify(ctx).writeAndFlush(eq(new ResponseMessage(1, OK)), any());
+        verify(ctx).writeAndFlush(any(), any());
     }
 
 }
