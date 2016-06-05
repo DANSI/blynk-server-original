074ab9c3354c3b71bea1f27ebc125a73129d10cb
Dmitriy Dumanskiy <doom369@gmail.com>
2016-06-05 14:27:43 +0300

    unused script removed

--- scripts/copy_to_azure_server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-scp ../server/tcp-server/target/server-0.4.2.jar azureuser@blynk-test-east.cloudapp.net:/home/azureuser
-scp ../server/administration/target/admin-0.4.2.jar azureuser@blynk-test-east.cloudapp.net:/home/azureuser
