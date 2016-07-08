43dddc352cd443c8f8816b3efe89792a3cf9bd53
Dmitriy Dumanskiy <doom369@gmail.com>
2016-07-08 20:26:14 +0300

    old todo removed

--- server/core/src/main/java/cc/blynk/server/core/dao/ReportingDao.java
+++ server/core/src/main/java/cc/blynk/server/core/dao/ReportingDao.java
@@ -55,7 +55,6 @@ public class ReportingDao {
         }
     }
 
-    //todo move out from event loop
     public static byte[] getAllFromDisk(String dataFolder, String username, int dashId, PinType pinType, byte pin, int count, GraphType type) {
         Path userDataFile = Paths.get(dataFolder, username, generateFilename(dashId, pinType, pin, type));
         if (Files.notExists(userDataFile)) {
