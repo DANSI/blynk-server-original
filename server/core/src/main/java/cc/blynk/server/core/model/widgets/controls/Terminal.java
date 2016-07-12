cb227fcb0b6251f8a04ba2549da10904eefb761e
Dmitriy Dumanskiy <doom369@gmail.com>
2016-07-12 19:09:38 +0300

    #200 send commands for console on activate

--- server/core/src/main/java/cc/blynk/server/core/model/widgets/controls/Terminal.java
+++ server/core/src/main/java/cc/blynk/server/core/model/widgets/controls/Terminal.java
@@ -3,8 +3,13 @@ package cc.blynk.server.core.model.widgets.controls;
 import cc.blynk.server.core.model.enums.PinType;
 import cc.blynk.server.core.model.widgets.OnePinWidget;
 import cc.blynk.utils.LimitedQueue;
+import cc.blynk.utils.StringUtils;
+import io.netty.channel.Channel;
 
 import java.util.List;
+
+import static cc.blynk.server.core.protocol.enums.Command.*;
+import static cc.blynk.utils.ByteBufUtil.*;
 
 /**
  * The Blynk Project.
@@ -27,6 +32,17 @@ public class Terminal extends OnePinWidget {
         if (isSame(pin, type)) {
             this.value = value;
             this.lastCommands.add(value);
+        }
+    }
+
+    @Override
+    public void sendSyncOnActivate(Channel appChannel, int dashId) {
+        if (pin == -1 || pinType == null || lastCommands.size() == 0) {
+            return;
+        }
+        for (String storedValue : lastCommands) {
+            String body = makeHardwareBody(pinType, pin, storedValue);
+            appChannel.write(makeStringMessage(SYNC, 1111, dashId + StringUtils.BODY_SEPARATOR_STRING + body));
         }
     }
 
