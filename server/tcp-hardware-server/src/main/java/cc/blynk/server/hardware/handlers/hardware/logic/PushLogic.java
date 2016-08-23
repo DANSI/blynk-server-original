d72fc6b38b0a730b5e201c188a558b7c5f99f4d9
Dmitriy Dumanskiy <doom369@gmail.com>
2016-08-24 02:55:29 +0300

    returned old code for consistency

--- server/tcp-hardware-server/src/main/java/cc/blynk/server/hardware/handlers/hardware/logic/PushLogic.java
+++ server/tcp-hardware-server/src/main/java/cc/blynk/server/hardware/handlers/hardware/logic/PushLogic.java
@@ -2,6 +2,7 @@ package cc.blynk.server.hardware.handlers.hardware.logic;
 
 import cc.blynk.server.core.model.DashBoard;
 import cc.blynk.server.core.model.widgets.notifications.Notification;
+import cc.blynk.server.core.protocol.exceptions.NoActiveDashboardException;
 import cc.blynk.server.core.protocol.exceptions.NotificationBodyInvalidException;
 import cc.blynk.server.core.protocol.model.messages.StringMessage;
 import cc.blynk.server.core.session.HardwareStateHolder;
@@ -11,8 +12,6 @@ import io.netty.channel.ChannelHandlerContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import static cc.blynk.server.core.protocol.enums.Response.NO_ACTIVE_DASHBOARD;
-import static cc.blynk.utils.ByteBufUtil.makeResponse;
 import static cc.blynk.utils.ByteBufUtil.ok;
 
 /**
@@ -43,9 +42,7 @@ public class PushLogic extends NotificationBase {
         DashBoard dash = state.user.profile.getDashByIdOrThrow(state.dashId);
 
         if (!dash.isActive) {
-            log.debug("No active dashboard.");
-            ctx.writeAndFlush(makeResponse(message.id, NO_ACTIVE_DASHBOARD), ctx.voidPromise());
-            return;
+            throw new NoActiveDashboardException(message.id);
         }
 
         Notification widget = dash.getWidgetByType(Notification.class);
