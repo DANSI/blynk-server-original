6efa59b3696500b3f734cc26cbeb3294af3e89df
Dmitriy Dumanskiy <doom369@gmail.com>
2016-08-30 20:42:15 +0300

    TimeInput now supports Sync.

--- server/core/src/main/java/cc/blynk/server/core/model/widgets/controls/TimeInput.java
+++ server/core/src/main/java/cc/blynk/server/core/model/widgets/controls/TimeInput.java
@@ -1,13 +1,17 @@
 package cc.blynk.server.core.model.widgets.controls;
 
 import cc.blynk.server.core.model.widgets.OnePinWidget;
+import io.netty.channel.ChannelHandlerContext;
+
+import static cc.blynk.server.core.protocol.enums.Command.HARDWARE;
+import static cc.blynk.utils.ByteBufUtil.makeStringMessage;
 
 /**
  * The Blynk Project.
  * Created by Dmitriy Dumanskiy.
  * Created on 27.08.16.
  */
-public class TimeInput extends OnePinWidget {
+public class TimeInput extends OnePinWidget implements HardwareSyncWidget {
 
     public String format;
 
@@ -30,12 +34,20 @@ public class TimeInput extends OnePinWidget {
     public boolean isTimezoneAllowed;
 
     @Override
+    public void send(ChannelHandlerContext ctx, int msgId) {
+        String body = makeHardwareBody();
+        if (body != null) {
+            ctx.write(makeStringMessage(HARDWARE, msgId, body), ctx.voidPromise());
+        }
+    }
+
+    @Override
     public String getModeType() {
         return "out";
     }
 
     @Override
     public int getPrice() {
-        return 0;
+        return 200;
     }
 }
