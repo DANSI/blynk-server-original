156ce92964afa8bb743e32712576a4f30a380f95
Dmitriy Dumanskiy <doom369@gmail.com>
2016-09-08 15:16:49 +0300

    #305 music player updates state based on hardware command

--- server/core/src/main/java/cc/blynk/server/core/model/widgets/others/Player.java
+++ server/core/src/main/java/cc/blynk/server/core/model/widgets/others/Player.java
@@ -1,15 +1,46 @@
 package cc.blynk.server.core.model.widgets.others;
 
+import cc.blynk.server.core.model.enums.PinType;
 import cc.blynk.server.core.model.widgets.OnePinWidget;
+import cc.blynk.server.core.model.widgets.controls.HardwareSyncWidget;
+import io.netty.channel.ChannelHandlerContext;
+
+import static cc.blynk.server.core.protocol.enums.Command.HARDWARE;
+import static cc.blynk.utils.ByteBufUtil.makeStringMessage;
 
 /**
  * The Blynk Project.
  * Created by Dmitriy Dumanskiy.
  * Created on 28.03.16.
  */
-public class Player extends OnePinWidget {
+public class Player extends OnePinWidget implements HardwareSyncWidget {
 
     public boolean isOnPlay;
+
+    @Override
+    public boolean updateIfSame(byte pin, PinType type, String value) {
+        if (isSame(pin, type)) {
+            this.value = value;
+            switch (value) {
+                case "play" :
+                    isOnPlay = true;
+                    break;
+                case "stop" :
+                    isOnPlay = false;
+                    break;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void send(ChannelHandlerContext ctx, int msgId) {
+        String body = makeHardwareBody();
+        if (body != null) {
+            ctx.write(makeStringMessage(HARDWARE, msgId, body), ctx.voidPromise());
+        }
+    }
 
     @Override
     public String getModeType() {
