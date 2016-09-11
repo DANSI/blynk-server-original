2c3828c6db7b386bf2d59e1a3489b8aff9f4914e
Dmitriy Dumanskiy <doom369@gmail.com>
2016-09-11 18:45:02 +0300

    removed old field from RTC

--- server/core/src/main/java/cc/blynk/server/core/model/widgets/others/rtc/RTC.java
+++ server/core/src/main/java/cc/blynk/server/core/model/widgets/others/rtc/RTC.java
@@ -4,17 +4,10 @@ import cc.blynk.server.core.model.Pin;
 import cc.blynk.server.core.model.widgets.OnePinWidget;
 import cc.blynk.server.core.model.widgets.controls.HardwareSyncWidget;
 import cc.blynk.utils.DateTimeUtils;
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JsonDeserializer;
-import com.fasterxml.jackson.databind.JsonSerializer;
-import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import io.netty.channel.ChannelHandlerContext;
 
-import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
@@ -28,11 +21,6 @@ import static cc.blynk.utils.ByteBufUtil.makeStringMessage;
  * Created on 21.03.15.
  */
 public class RTC extends OnePinWidget implements HardwareSyncWidget {
-
-    //todo remove in future updates
-    @JsonSerialize(using = ZoneOffsetToString.class)
-    @JsonDeserialize(using = StringToZoneOffset.class, as = ZoneOffset.class)
-    public ZoneOffset timezone;
 
     @JsonSerialize(using = ZoneIdToString.class)
     @JsonDeserialize(using = StringToZoneId.class, as = ZoneId.class)
@@ -53,28 +41,6 @@ public class RTC extends OnePinWidget implements HardwareSyncWidget {
         return 100;
     }
 
-    private static class ZoneOffsetToString extends JsonSerializer<Object> {
-
-        @Override
-        public void serialize(Object value, JsonGenerator jsonGenerator, SerializerProvider serializers) throws IOException {
-            String result = value.toString();
-            if (result.equals("Z")) {
-                result = "+00:00";
-            }
-            jsonGenerator.writeObject(result);
-        }
-
-    }
-
-    private static class StringToZoneOffset extends JsonDeserializer<ZoneOffset> {
-
-        @Override
-        public ZoneOffset deserialize(JsonParser p, DeserializationContext ctx) throws IOException {
-            return ZoneOffset.of(p.readValueAs(String.class));
-        }
-
-    }
-
     @Override
     public boolean isRequiredForSyncAll() {
         return false;
@@ -90,8 +56,6 @@ public class RTC extends OnePinWidget implements HardwareSyncWidget {
         ZoneId zone;
         if (tzName != null) {
             zone = tzName;
-        } else if (timezone != null) {
-            zone = timezone;
         } else {
             zone = DateTimeUtils.UTC;
         }
