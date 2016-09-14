2892be454453315c58d5a8c624601dbb27d3094b
Dmitriy Dumanskiy <doom369@gmail.com>
2016-09-14 22:29:52 +0300

    #318 no tracking urls in body

--- server/notifications/email/src/main/java/cc/blynk/server/notifications/mail/http/MailBody.java
+++ server/notifications/email/src/main/java/cc/blynk/server/notifications/mail/http/MailBody.java
@@ -7,12 +7,14 @@ package cc.blynk.server.notifications.mail.http;
  */
 class MailBody {
 
+    public final Options options;
     public final Content content;
     public final Recipient[] recipients;
 
     public MailBody(String from, String subj, String body, String to, boolean isHtml) {
         this.content = new Content(from, subj, body, isHtml);
         this.recipients = new Recipient[]{new Recipient(to)};
+        this.options = new Options();
     }
 
 }
