9d6cf7c4a2c65c639ff6215da9013de9e05a75e1
Dmitriy Dumanskiy <doom369@gmail.com>
2016-09-22 21:54:12 +0300

    #332 changed facebook login handler.

--- scripts/copy_to_azure_qa.sh
+++ scripts/copy_to_azure_qa.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-scp ../server/launcher/target/server-0.18.2-SNAPSHOT.jar azureuser@blynk-qa.cloudapp.net:/home/azureuser
+scp ../server/launcher/target/server-0.18.3-SNAPSHOT.jar azureuser@blynk-qa.cloudapp.net:/home/azureuser
