62980fc1d7a5f74d0bc6e43ae349c40f1bf384fb
Dmitriy Dumanskiy <doom369@gmail.com>
2016-09-22 21:45:29 +0300

    #332 changed facebook login handler.

--- server/tcp-app-server/src/main/java/cc/blynk/server/application/handlers/main/auth/FacebookLoginCheck.java
+++ server/tcp-app-server/src/main/java/cc/blynk/server/application/handlers/main/auth/FacebookLoginCheck.java
@@ -3,14 +3,9 @@ package cc.blynk.server.application.handlers.main.auth;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectReader;
-import org.apache.http.HttpEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
-
-import java.io.IOException;
+import org.asynchttpclient.AsyncCompletionHandler;
+import org.asynchttpclient.DefaultAsyncHttpClient;
+import org.asynchttpclient.Response;
 
 /**
  * Simple class that verifies that facebook access token is correct and belongs to user that sent it.
@@ -24,34 +19,36 @@ public class FacebookLoginCheck {
 
     private static final String URL = "https://graph.facebook.com/me?fields=email&access_token=TOKEN";
 
-    private final CloseableHttpClient httpclient;
+    private final DefaultAsyncHttpClient asyncHttpClient;
     private final ObjectReader facebookResponseReader = new ObjectMapper()
             .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-            .reader(FacebookVerifyResponse.class);
+            .readerFor(FacebookVerifyResponse.class);
 
-    public FacebookLoginCheck() {
-        this.httpclient = HttpClients.createDefault();
+    public FacebookLoginCheck(DefaultAsyncHttpClient asyncHttpClient) {
+        this.asyncHttpClient = asyncHttpClient;
     }
 
-    @SuppressWarnings("unchecked")
     public void verify(String username, String token) throws Exception {
-        HttpGet httpGet = new HttpGet(URL.replace("TOKEN", token));
+        asyncHttpClient.prepareGet(URL.replace("TOKEN", token))
+                .execute(new AsyncCompletionHandler<Response>() {
+                    @Override
+                    public Response onCompleted(Response response) throws Exception {
+                        if (response.getStatusCode() == 200) {
+                            FacebookVerifyResponse facebookVerifyResponse = facebookResponseReader.readValue(response.getResponseBody());
+                            if (!username.equals(facebookVerifyResponse.email)) {
+                                throw new IllegalArgumentException("Token is invalid. Facebook email " + facebookVerifyResponse.email + " != " + username);
+                            }
+                        } else {
+                            throw new IllegalArgumentException(response.getResponseBody());
+                        }
+                        return response;
+                    }
 
-        try (CloseableHttpResponse response = httpclient.execute(httpGet)) {
-            HttpEntity entity = response.getEntity();
-            String msg = EntityUtils.toString(entity);
-            if (response.getStatusLine().getStatusCode() == 200) {
-                FacebookVerifyResponse facebookVerifyResponse = facebookResponseReader.readValue(msg);
-                if (!username.equals(facebookVerifyResponse.email)) {
-                    throw new IllegalArgumentException("Token is invalid. Facebook email " + facebookVerifyResponse.email + " != " + username);
-                }
-            } else {
-                EntityUtils.consume(entity);
-                throw new IOException(msg);
-            }
-        } finally {
-            httpGet.releaseConnection();
-        }
+                    @Override
+                    public void onThrowable(Throwable t) {
+                        throw new IllegalArgumentException(t);
+                    }
+                });
     }
 
     private static class FacebookVerifyResponse {
