db60fd5d0bd9c4fea6d0e41e226c61e4ca6e0f20
Dmitriy Dumanskiy <doom369@gmail.com>
2016-10-13 17:31:11 +0300

    map widget support

--- server/core/src/main/java/cc/blynk/server/core/model/widgets/outputs/Map.java
+++ server/core/src/main/java/cc/blynk/server/core/model/widgets/outputs/Map.java
@@ -11,6 +11,8 @@ public class Map extends OnePinWidget {
 
     public boolean isPinToLatestPoint;
 
+    public String labelFormat;
+
     public int radius; //zoom level / radius which user selected.
 
     public float lat; // last user position on map
