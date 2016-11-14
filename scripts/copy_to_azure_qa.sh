a8a708e5090b4066f91a4040a185c03bbdd1019c
Dmitriy Dumanskiy <doom369@gmail.com>
2016-11-14 22:27:32 +0200

    new version

--- scripts/copy_to_azure_qa.sh
+++ scripts/copy_to_azure_qa.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-scp ../server/launcher/target/server-0.19.1-SNAPSHOT.jar azureuser@blynk-qa.cloudapp.net:/home/azureuser
+scp ../server/launcher/target/server-0.20.0-SNAPSHOT.jar azureuser@blynk-qa.cloudapp.net:/home/azureuser
