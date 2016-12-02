a1b4a41e2c0f8188044aa8fb9c9acdaaca2290b6
Dmitriy Dumanskiy <doom369@gmail.com>
2016-12-02 21:34:02 +0200

    #383 storage for map widget

--- server/core/src/main/java/cc/blynk/server/core/model/widgets/outputs/Map.java
+++ server/core/src/main/java/cc/blynk/server/core/model/widgets/outputs/Map.java
@@ -1,6 +1,15 @@
 package cc.blynk.server.core.model.widgets.outputs;
 
+import cc.blynk.server.core.model.enums.PinType;
 import cc.blynk.server.core.model.widgets.OnePinWidget;
+import cc.blynk.utils.ParseUtil;
+import cc.blynk.utils.structure.LimitedArrayDeque;
+import io.netty.channel.Channel;
+
+import static cc.blynk.server.core.protocol.enums.Command.SYNC;
+import static cc.blynk.utils.BlynkByteBufUtil.makeUTF8StringMessage;
+import static cc.blynk.utils.StringUtils.makeBody;
+
 
 /**
  * The Blynk Project.
@@ -8,6 +17,9 @@ import cc.blynk.server.core.model.widgets.OnePinWidget;
  * Created on 21.03.15.
  */
 public class Map extends OnePinWidget {
+
+    private static final int POOL_SIZE = ParseUtil.parseInt(System.getProperty("map.strings.pool.size", "25"));
+    private transient final LimitedArrayDeque<String> lastCommands = new LimitedArrayDeque<>(POOL_SIZE);
 
     public boolean isPinToLatestPoint;
 
@@ -21,7 +33,25 @@ public class Map extends OnePinWidget {
 
     public float lon; // last user position on map
 
-    public Point[] points;
+    @Override
+    public boolean updateIfSame(int deviceId, byte pin, PinType type, String value) {
+        if (isSame(deviceId, pin, type)) {
+            this.lastCommands.add(value);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void sendSyncOnActivate(Channel appChannel, int dashId) {
+        if (pin == -1 || pinType == null || lastCommands.size() == 0) {
+            return;
+        }
+        for (String storedValue : lastCommands) {
+            String body = makeBody(dashId, deviceId, makeHardwareBody(pinType, pin, storedValue));
+            appChannel.write(makeUTF8StringMessage(SYNC, 1111, body));
+        }
+    }
 
     @Override
     public String getModeType() {
