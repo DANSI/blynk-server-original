7443b0f9e6ea4bd489823eaca49a75e51a4666d4
Dmitriy Dumanskiy <doom369@gmail.com>
2016-12-20 13:40:42 +0200

    new version

--- integration-tests/src/test/java/cc/blynk/integration/tcp/MainWorkflowTest.java
+++ integration-tests/src/test/java/cc/blynk/integration/tcp/MainWorkflowTest.java
@@ -652,8 +652,8 @@ public class MainWorkflowTest extends IntegrationBase {
                 "-\n" +
                 "Getting Started Guide -> http://www.blynk.cc/getting-started\n" +
                 "Documentation -> http://docs.blynk.cc/\n" +
-                "Latest Blynk library -> https://github.com/blynkkk/blynk-library/releases/download/v0.4.1/Blynk_Release_v0.4.1.zip\n" +
-                "Latest Blynk server -> https://github.com/blynkkk/blynk-server/releases/download/v0.20.2/server-0.20.2.jar\n" +
+                "Latest Blynk library -> https://github.com/blynkkk/blynk-library/releases/download/v0.4.2/Blynk_Release_v0.4.2.zip\n" +
+                "Latest Blynk server -> https://github.com/blynkkk/blynk-server/releases/download/v0.21.0/server-0.21.0.jar\n" +
                 "-\n" +
                 "http://www.blynk.cc\n" +
                 "twitter.com/blynk_app\n" +
