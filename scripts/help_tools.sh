be6af51c719a079aa583abd9f6ba744c4435bf95
Dmitriy Dumanskiy <doom369@gmail.com>
2017-01-16 19:49:08 +0200

    useful tools

--- scripts/help_tools.sh
+++ scripts/help_tools.sh
@@ -11,3 +11,8 @@ wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key a
 sudo apt-get update
 sudo apt-get install postgresql postgresql-contrib
 sudo su - postgres
+
+apt-get install ipset
+ipset create blacklist hash:ip hashsize 4096
+iptables -I INPUT -m set --match-set blacklist src -j DROP
+iptables -I FORWARD -m set --match-set blacklist src -j DROP
