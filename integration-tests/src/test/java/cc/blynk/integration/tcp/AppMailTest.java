ecd21c8a9b24fb7d2be48ba1ecf422ec71101b82
Dmitriy Dumanskiy <doom369@gmail.com>
2017-04-09 23:10:06 +0300

    new library version

--- integration-tests/src/test/java/cc/blynk/integration/tcp/AppMailTest.java
+++ integration-tests/src/test/java/cc/blynk/integration/tcp/AppMailTest.java
@@ -94,7 +94,7 @@ public class AppMailTest extends IntegrationBase {
                 "Documentation -> http://docs.blynk.cc/\n" +
                 "Sketch generator -> http://examples.blynk.cc/\n" +
                 "\n" +
-                "Latest Blynk library -> https://github.com/blynkkk/blynk-library/releases/download/v0.4.6/Blynk_Release_v0.4.6.zip\n" +
+                "Latest Blynk library -> https://github.com/blynkkk/blynk-library/releases/download/v0.4.7/Blynk_Release_v0.4.7.zip\n" +
                 "Latest Blynk server -> https://github.com/blynkkk/blynk-server/releases/download/v0.23.5/server-0.23.5.jar\n" +
                 "-\n" +
                 "http://www.blynk.cc\n" +
@@ -136,7 +136,7 @@ public class AppMailTest extends IntegrationBase {
                 "Documentation -> http://docs.blynk.cc/\n" +
                 "Sketch generator -> http://examples.blynk.cc/\n" +
                 "\n" +
-                "Latest Blynk library -> https://github.com/blynkkk/blynk-library/releases/download/v0.4.6/Blynk_Release_v0.4.6.zip\n" +
+                "Latest Blynk library -> https://github.com/blynkkk/blynk-library/releases/download/v0.4.7/Blynk_Release_v0.4.7.zip\n" +
                 "Latest Blynk server -> https://github.com/blynkkk/blynk-server/releases/download/v0.23.5/server-0.23.5.jar\n" +
                 "-\n" +
                 "http://www.blynk.cc\n" +
