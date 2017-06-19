997bf47f55b05e6b10a64a3a9ffd4edde6c6f34f
Dmitriy Dumanskiy <doom369@gmail.com>
2017-06-19 17:49:07 +0300

    #651 start error fix for outdated data

--- server/core/src/main/java/cc/blynk/server/core/model/PinStorageKeyDeserializer.java
+++ server/core/src/main/java/cc/blynk/server/core/model/PinStorageKeyDeserializer.java
@@ -1,6 +1,7 @@
 package cc.blynk.server.core.model;
 
 import cc.blynk.server.core.model.enums.PinType;
+import cc.blynk.server.core.protocol.exceptions.ParseException;
 import cc.blynk.utils.ParseUtil;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.KeyDeserializer;
@@ -21,7 +22,13 @@ public class PinStorageKeyDeserializer extends KeyDeserializer {
 
         int deviceId = ParseUtil.parseInt(split[0]);
         PinType pinType = PinType.getPinType(split[1].charAt(0));
-        byte pin = ParseUtil.parseByte(split[1].substring(1, split[1].length()));
+        byte pin = 0;
+        try {
+            pin = ParseUtil.parseByte(split[1].substring(1, split[1].length()));
+        } catch (ParseException e) {
+            //special case for outdated data format.
+            return new PinStorageKey(deviceId, pinType, pin);
+        }
         if (split.length == 3) {
             return new PinPropertyStorageKey(deviceId, pinType, pin, split[2]);
         } else {
