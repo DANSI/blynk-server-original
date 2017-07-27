46605d0f4dda18e2c54f41fdaf5fb62a02804fe9
Dmitriy Dumanskiy <doom369@gmail.com>
2017-07-27 11:52:58 +0300

    static imports for GraphPeriod

--- server/core/src/main/java/cc/blynk/server/core/model/widgets/outputs/graph/GraphPeriod.java
+++ server/core/src/main/java/cc/blynk/server/core/model/widgets/outputs/graph/GraphPeriod.java
@@ -1,5 +1,8 @@
 package cc.blynk.server.core.model.widgets.outputs.graph;
 
+import static cc.blynk.server.core.model.widgets.outputs.graph.GraphGranularityType.HOURLY;
+import static cc.blynk.server.core.model.widgets.outputs.graph.GraphGranularityType.MINUTE;
+
 /**
  * The Blynk Project.
  * Created by Dmitriy Dumanskiy.
@@ -7,14 +10,14 @@ package cc.blynk.server.core.model.widgets.outputs.graph;
  */
 public enum GraphPeriod {
 
-    LIVE(60, GraphGranularityType.MINUTE),
-    ONE_HOUR(60, GraphGranularityType.MINUTE),
-    SIX_HOURS(6 * 60, GraphGranularityType.MINUTE),
-    DAY(24 * 60, GraphGranularityType.MINUTE),
-    WEEK(24 * 7, GraphGranularityType.HOURLY),
-    MONTH(30 * 24, GraphGranularityType.HOURLY),
-    THREE_MONTHS(3 * 30 * 24, GraphGranularityType.HOURLY),
-    ALL(12 * 30 * 24, GraphGranularityType.HOURLY);
+    LIVE(60, MINUTE),
+    ONE_HOUR(60, MINUTE),
+    SIX_HOURS(6 * 60, MINUTE),
+    DAY(24 * 60, MINUTE),
+    WEEK(24 * 7, HOURLY),
+    MONTH(30 * 24, HOURLY),
+    THREE_MONTHS(3 * 30 * 24, HOURLY),
+    ALL(12 * 30 * 24, HOURLY);
 
     public final int numberOfPoints;
     public final GraphGranularityType granularityType;
