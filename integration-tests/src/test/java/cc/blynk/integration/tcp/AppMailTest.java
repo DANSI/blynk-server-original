fcf18284cf554ce84fe9b5036b89690e00e6cb9c
Dmitriy Dumanskiy <doom369@gmail.com>
2017-09-04 20:09:14 +0300

    version up

--- integration-tests/src/test/java/cc/blynk/integration/tcp/AppMailTest.java
+++ integration-tests/src/test/java/cc/blynk/integration/tcp/AppMailTest.java
@@ -104,7 +104,7 @@ public class AppMailTest extends IntegrationBase {
                 "Sketch generator -> https://examples.blynk.cc/\n" +
                 "\n" +
                 "Latest Blynk library -> https://github.com/blynkkk/blynk-library/releases/download/v0.4.8/Blynk_Release_v0.4.8.zip\n" +
-                "Latest Blynk server -> https://github.com/blynkkk/blynk-server/releases/download/v0.27.0/server-0.27.0.jar\n" +
+                "Latest Blynk server -> https://github.com/blynkkk/blynk-server/releases/download/v0.27.1/server-0.27.1.jar\n" +
                 "-\n" +
                 "https://www.blynk.cc\n" +
                 "twitter.com/blynk_app\n" +
@@ -146,7 +146,7 @@ public class AppMailTest extends IntegrationBase {
                 "Sketch generator -> https://examples.blynk.cc/\n" +
                 "\n" +
                 "Latest Blynk library -> https://github.com/blynkkk/blynk-library/releases/download/v0.4.8/Blynk_Release_v0.4.8.zip\n" +
-                "Latest Blynk server -> https://github.com/blynkkk/blynk-server/releases/download/v0.27.0/server-0.27.0.jar\n" +
+                "Latest Blynk server -> https://github.com/blynkkk/blynk-server/releases/download/v0.27.1/server-0.27.1.jar\n" +
                 "-\n" +
                 "https://www.blynk.cc\n" +
                 "twitter.com/blynk_app\n" +
--- new_server_install.md
+++ new_server_install.md
@@ -6,7 +6,7 @@
         sudo apt-get update
         sudo apt-get install oracle-java8-installer
         
-        wget "https://github.com/blynkkk/blynk-server/releases/download/v0.27.0/server-0.27.0.jar"
+        wget "https://github.com/blynkkk/blynk-server/releases/download/v0.27.1/server-0.27.1.jar"
         
 
 server.properties
