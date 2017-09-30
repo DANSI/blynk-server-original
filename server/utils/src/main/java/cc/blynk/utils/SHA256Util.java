76ddaae4b81181eb0da8011cc1c1cb7d552a7b2b
Dmitriy Dumanskiy <doom369@gmail.com>
2017-09-30 17:24:46 +0300

    #746 moved

--- server/utils/src/main/java/cc/blynk/utils/SHA256Util.java
+++ server/utils/src/main/java/cc/blynk/utils/SHA256Util.java
@@ -1,10 +1,10 @@
 package cc.blynk.utils;
 
-import io.netty.util.CharsetUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
@@ -24,7 +24,7 @@ public final class SHA256Util {
     public static String makeHash(String password, String salt) {
         try {
             MessageDigest md = MessageDigest.getInstance("SHA-256");
-            md.update(password.getBytes(CharsetUtil.UTF_8));
+            md.update(password.getBytes(StandardCharsets.UTF_8));
 
             byte[] byteData = md.digest(makeHash(salt.toLowerCase()));
 
@@ -37,7 +37,7 @@ public final class SHA256Util {
     }
 
     private static byte[] makeHash(String val) throws NoSuchAlgorithmException, UnsupportedEncodingException {
-        return MessageDigest.getInstance("SHA-256").digest(val.getBytes(CharsetUtil.UTF_8));
+        return MessageDigest.getInstance("SHA-256").digest(val.getBytes(StandardCharsets.UTF_8));
     }
 
 }
