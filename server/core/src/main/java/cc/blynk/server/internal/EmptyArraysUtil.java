fce36c249ed295531ffeb29198c6abcf729adbea
Dmitriy Dumanskiy <doom369@gmail.com>
2017-10-02 22:51:51 +0300

    #742 addtional empty arrays

--- server/core/src/main/java/cc/blynk/server/internal/EmptyArraysUtil.java
+++ server/core/src/main/java/cc/blynk/server/internal/EmptyArraysUtil.java
@@ -6,6 +6,8 @@ import cc.blynk.server.core.model.device.Device;
 import cc.blynk.server.core.model.device.Tag;
 import cc.blynk.server.core.model.widgets.Widget;
 import cc.blynk.server.core.model.widgets.outputs.graph.GraphDataStream;
+import cc.blynk.server.core.model.widgets.ui.tiles.Tile;
+import cc.blynk.server.core.model.widgets.ui.tiles.TileTemplate;
 
 /**
  * The Blynk Project.
@@ -22,6 +24,8 @@ public final class EmptyArraysUtil {
     public static final Tag[] EMPTY_TAGS = {};
     public static final Device[] EMPTY_DEVICES = {};
     public static final Widget[] EMPTY_WIDGETS = {};
+    public static final TileTemplate[] EMPTY_TEMPLATES = {};
+    public static final Tile[] EMPTY_TILES = {};
     public static final byte[] EMPTY_BYTES = {};
     public static final App[] EMPTY_APPS = {};
     public static final GraphDataStream[] EMPTY_GRAPH_DATA_STREAMS = {};
