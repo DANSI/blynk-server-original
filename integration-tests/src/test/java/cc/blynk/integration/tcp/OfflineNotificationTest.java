dd06ca95bbcc1689beda9b1f8a30de6ff357e5d3
Dmitriy Dumanskiy <doom369@gmail.com>
2017-12-06 20:24:53 +0200

    additional tests for new offline message with multiple device

---
+++ integration-tests/src/test/java/cc/blynk/integration/tcp/OfflineNotificationTest.java
@@ -0,0 +1,135 @@
+package cc.blynk.integration.tcp;
+
+import cc.blynk.integration.IntegrationBase;
+import cc.blynk.integration.model.tcp.ClientPair;
+import cc.blynk.integration.model.tcp.TestHardClient;
+import cc.blynk.server.application.AppServer;
+import cc.blynk.server.core.BaseServer;
+import cc.blynk.server.core.model.device.Device;
+import cc.blynk.server.core.model.device.Status;
+import cc.blynk.server.core.model.serialization.JsonParser;
+import cc.blynk.server.core.protocol.model.messages.appllication.CreateDevice;
+import cc.blynk.server.core.protocol.model.messages.appllication.DeviceOfflineMessage;
+import cc.blynk.server.hardware.HardwareServer;
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.timeout;
+import static org.mockito.Mockito.verify;
+
+/**
+ * The Blynk Project.
+ * Created by Dmitriy Dumanskiy.
+ * Created on 2/2/2015.
+ *
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class OfflineNotificationTest extends IntegrationBase {
+
+    private BaseServer appServer;
+    private BaseServer hardwareServer;
+    private ClientPair clientPair;
+
+    @Before
+    public void init() throws Exception {
+        this.hardwareServer = new HardwareServer(holder).start();
+        this.appServer = new AppServer(holder).start();
+
+        this.clientPair = initAppAndHardPair("user_profile_json_empty_dash.txt");
+    }
+
+    @After
+    public void shutdown() {
+        this.appServer.close();
+        this.hardwareServer.close();
+        this.clientPair.stop();
+    }
+
+    @Test
+    public void testOfflineTimingIsCorrectForMultipleDevices() throws Exception {
+        Device device2 = new Device(1, "My Device", "ESP8266");
+        device2.status = Status.OFFLINE;
+
+        clientPair.appClient.send("createDevice 1\0" + device2.toString());
+        String createdDevice = clientPair.appClient.getBody();
+        Device device = JsonParser.parseDevice(createdDevice);
+        assertNotNull(device);
+        assertNotNull(device.token);
+        verify(clientPair.appClient.responseMock, timeout(500)).channelRead(any(), eq(new CreateDevice(1, device.toString())));
+
+        clientPair.appClient.send("getDevices 1");
+        String response = clientPair.appClient.getBody(2);
+
+        Device[] devices = JsonParser.MAPPER.readValue(response, Device[].class);
+        TestCase.assertNotNull(devices);
+        assertEquals(2, devices.length);
+
+        assertEquals(1, devices[1].id);
+
+        TestHardClient hardClient2 = new TestHardClient("localhost", tcpHardPort);
+        hardClient2.start();
+        hardClient2.send("login " + devices[1].token);
+        verify(hardClient2.responseMock, timeout(500)).channelRead(any(), eq(ok(1)));
+
+        hardClient2.send("internal " + b("ver 0.3.1 h-beat 10 buff-in 256 dev Arduino cpu ATmega328P con W5100"));
+        verify(hardClient2.responseMock, timeout(500)).channelRead(any(), eq(ok(2)));
+
+        clientPair.hardwareClient.stop();
+
+        verify(clientPair.appClient.responseMock, timeout(500)).channelRead(any(),
+                eq(new DeviceOfflineMessage(0, b("1-0"))));
+        verify(clientPair.appClient.responseMock, never()).channelRead(any(),
+                eq(new DeviceOfflineMessage(0, b("1-1"))));
+
+        clientPair.appClient.reset();
+        hardClient2.stop();
+
+        verify(clientPair.appClient.responseMock, timeout(500)).channelRead(any(), eq(new DeviceOfflineMessage(0, b("1-1"))));
+        verify(clientPair.appClient.responseMock, never()).channelRead(any(), eq(new DeviceOfflineMessage(0, b("1-0"))));
+    }
+
+    @Test
+    public void testOfflineTimingIsCorrectForMultipleDevices2() throws Exception {
+        Device device2 = new Device(1, "My Device", "ESP8266");
+        device2.status = Status.OFFLINE;
+
+        clientPair.appClient.send("createDevice 1\0" + device2.toString());
+        String createdDevice = clientPair.appClient.getBody();
+        Device device = JsonParser.parseDevice(createdDevice);
+        TestCase.assertNotNull(device);
+        TestCase.assertNotNull(device.token);
+        verify(clientPair.appClient.responseMock, timeout(500)).channelRead(any(), eq(new CreateDevice(1, device.toString())));
+
+        clientPair.appClient.send("getDevices 1");
+        String response = clientPair.appClient.getBody(2);
+
+        Device[] devices = JsonParser.MAPPER.readValue(response, Device[].class);
+        TestCase.assertNotNull(devices);
+        assertEquals(2, devices.length);
+
+        assertEquals(1, devices[1].id);
+
+        TestHardClient hardClient2 = new TestHardClient("localhost", tcpHardPort);
+        hardClient2.start();
+        hardClient2.send("login " + devices[1].token);
+        verify(hardClient2.responseMock, timeout(500)).channelRead(any(), eq(ok(1)));
+
+        hardClient2.send("internal " + b("ver 0.3.1 h-beat 1 buff-in 256 dev Arduino cpu ATmega328P con W5100"));
+        verify(hardClient2.responseMock, timeout(500)).channelRead(any(), eq(ok(2)));
+
+        clientPair.hardwareClient.stop();
+        hardClient2.stop();
+
+        verify(clientPair.appClient.responseMock, timeout(5000).times(1)).channelRead(any(), eq(new DeviceOfflineMessage(0, b("1-0"))));
+        verify(clientPair.appClient.responseMock, timeout(5000).times(1)).channelRead(any(), eq(new DeviceOfflineMessage(0, b("1-1"))));
+    }
+}
