f0d31f41d2083de0ca36db0c99e09680eccdd79a
Dmitriy Dumanskiy <doom369@gmail.com>
2018-02-12 17:39:17 +0200

    version up

--- integration-tests/src/test/java/cc/blynk/integration/tcp/AppMailTest.java
+++ integration-tests/src/test/java/cc/blynk/integration/tcp/AppMailTest.java
@@ -97,7 +97,7 @@ public class AppMailTest extends IntegrationBase {
                 "Sketch generator -> https://examples.blynk.cc/\n" +
                 "\n" +
                 "Latest Blynk library -> https://github.com/blynkkk/blynk-library/releases/download/v0.5.0/Blynk_Release_v0.5.0.zip\n" +
-                "Latest Blynk server -> https://github.com/blynkkk/blynk-server/releases/download/v0.31.0/server-0.31.0.jar\n" +
+                "Latest Blynk server -> https://github.com/blynkkk/blynk-server/releases/download/v0.31.1/server-0.31.1.jar\n" +
                 "-\n" +
                 "https://www.blynk.cc\n" +
                 "twitter.com/blynk_app\n" +
@@ -137,7 +137,7 @@ public class AppMailTest extends IntegrationBase {
                 "Sketch generator -> https://examples.blynk.cc/\n" +
                 "\n" +
                 "Latest Blynk library -> https://github.com/blynkkk/blynk-library/releases/download/v0.5.0/Blynk_Release_v0.5.0.zip\n" +
-                "Latest Blynk server -> https://github.com/blynkkk/blynk-server/releases/download/v0.31.0/server-0.31.0.jar\n" +
+                "Latest Blynk server -> https://github.com/blynkkk/blynk-server/releases/download/v0.31.1/server-0.31.1.jar\n" +
                 "-\n" +
                 "https://www.blynk.cc\n" +
                 "twitter.com/blynk_app\n" +
--- scripts/copy_to_azure_qa.sh
+++ scripts/copy_to_azure_qa.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-scp ../server/launcher/target/server-0.31.1-SNAPSHOT-java9.jar azureuser@blynk-qa.cloudapp.net:/home/azureuser
+scp ../server/launcher/target/server-0.32.0-SNAPSHOT-java9.jar azureuser@blynk-qa.cloudapp.net:/home/azureuser
