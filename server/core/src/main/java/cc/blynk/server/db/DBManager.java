e8e337bb45a593fc6366a3222a7d1130685e5837
Dmitriy Dumanskiy <doom369@gmail.com>
2018-02-17 12:37:42 +0200

    DB pool size increased to 5

--- server/core/src/main/java/cc/blynk/server/db/DBManager.java
+++ server/core/src/main/java/cc/blynk/server/db/DBManager.java
@@ -131,7 +131,7 @@ public class DBManager implements Closeable {
 
         config.setAutoCommit(false);
         config.setConnectionTimeout(serverProperties.getLongProperty("connection.timeout.millis"));
-        config.setMaximumPoolSize(3);
+        config.setMaximumPoolSize(5);
         config.setMaxLifetime(0);
         config.setConnectionTestQuery("SELECT 1");
         return config;
