ef166430b902c61252d2304e1a6ef58d3272cbb7
Dmitriy Dumanskiy <doom369@gmail.com>
2018-03-11 18:49:50 +0200

    one more test for bridge

--- integration-tests/src/test/java/cc/blynk/integration/tcp/BridgeWorkflowTest.java
+++ integration-tests/src/test/java/cc/blynk/integration/tcp/BridgeWorkflowTest.java
@@ -4,6 +4,7 @@ import cc.blynk.integration.IntegrationBase;
 import cc.blynk.integration.model.tcp.ClientPair;
 import cc.blynk.integration.model.tcp.TestAppClient;
 import cc.blynk.integration.model.tcp.TestHardClient;
+import cc.blynk.server.core.model.DashBoard;
 import cc.blynk.server.core.model.device.Device;
 import cc.blynk.server.core.model.device.Status;
 import cc.blynk.server.core.model.enums.PinType;
@@ -398,4 +399,33 @@ public class BridgeWorkflowTest extends IntegrationBase {
         clientPair.hardwareClient.verifyResult(notAllowed(1));
     }
 
+    @Test
+    public void testCorrectWorkflow2HardsOnDifferentProjects() throws Exception {
+        DashBoard dash = new DashBoard();
+        dash.id = 5;
+        dash.name = "test";
+        dash.activate();
+        clientPair.appClient.createDash(dash);
+        clientPair.appClient.verifyResult(ok(1));
+
+        Device device = new Device(1, "My Device", "ESP8266");
+        clientPair.appClient.createDevice(dash.id, device);
+        device = clientPair.appClient.getDevice(2);
+
+        //creating 1 new hard client
+        TestHardClient hardClient1 = new TestHardClient("localhost", tcpHardPort);
+        hardClient1.start();
+        hardClient1.login(device.token);
+        hardClient1.verifyResult(ok(1));
+        hardClient1.reset();
+
+        clientPair.hardwareClient.send("bridge 1 i " + device.token);
+        clientPair.hardwareClient.verifyResult(ok(1));
+        clientPair.hardwareClient.send("bridge 1 vw 11 11");
+        hardClient1.verifyResult(bridge(2, "vw 11 11"));
+        clientPair.appClient.verifyResult(hardwareConnected(1, "5-1"));
+        clientPair.appClient.verifyResult(hardware(2, "5-1 vw 11 11"));
+        clientPair.appClient.never(hardware(2, "5-0 vw 11 11"));
+        clientPair.appClient.never(hardware(2, "1-0 vw 11 11"));
+    }
 }
