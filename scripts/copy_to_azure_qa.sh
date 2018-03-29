77fd0bc395abfb0102fc86984c958b782abe3e6d
Dmitriy Dumanskiy <doom369@gmail.com>
2018-03-29 12:53:54 +0300

    new qa server

--- scripts/copy_to_azure_qa.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-scp ../server/launcher/target/server-0.34.0-SNAPSHOT-java9.jar azureuser@blynk-qa.cloudapp.net:/home/azureuser
---
+++ scripts/copy_to_qa.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+scp ../server/launcher/target/server-0.34.0-SNAPSHOT-java9.jar root@qa.blynk.cc:/root
