f85a7b487f209b46b7c2f177ed17fe64c019d6b8
Dmitriy Dumanskiy <doom369@gmail.com>
2018-04-08 19:38:25 +0300

    version up

--- scripts/copy_to_qa.sh
+++ scripts/copy_to_qa.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-scp ../server/launcher/target/server-0.34.1-SNAPSHOT-java9.jar root@qa.blynk.cc:/root
+scp ../server/launcher/target/server-0.35.0-SNAPSHOT-java9.jar root@qa.blynk.cc:/root
