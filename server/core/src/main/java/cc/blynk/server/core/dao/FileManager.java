8006e1287f69a7d00f1297528c3870c82e77da46
Dmitriy Dumanskiy <doom369@gmail.com>
2018-05-31 10:06:26 +0300

    #1075 back compatibility fix

--- server/core/src/main/java/cc/blynk/server/core/dao/FileManager.java
+++ server/core/src/main/java/cc/blynk/server/core/dao/FileManager.java
@@ -213,6 +213,9 @@ public class FileManager {
             if (dashBoard.devices != null) {
                 for (Device device : dashBoard.devices) {
                     device.status = Status.OFFLINE;
+                    if (device.firstConnectTime == 0){
+                        device.firstConnectTime = device.connectTime;
+                    }
                 }
             }
 
