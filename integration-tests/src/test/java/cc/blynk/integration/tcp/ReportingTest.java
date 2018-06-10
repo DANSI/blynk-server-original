60a99b85cb0f34eecd391a522c9d028542f8115c
Dmitriy Dumanskiy <doom369@gmail.com>
2018-06-10 17:48:33 +0300

    possible failure for test fix

--- integration-tests/src/test/java/cc/blynk/integration/tcp/ReportingTest.java
+++ integration-tests/src/test/java/cc/blynk/integration/tcp/ReportingTest.java
@@ -572,7 +572,7 @@ public class ReportingTest extends IntegrationBase {
 
         report = clientPair.appClient.parseReportFromResponse(2);
         assertNotNull(report);
-        assertEquals(System.currentTimeMillis(), report.nextReportAt, 2000);
+        assertEquals(System.currentTimeMillis(), report.nextReportAt, 2500);
 
         String date = LocalDate.now(report.tzName).toString();
         String filename = DEFAULT_TEST_USER + "_Blynk_" + report.id + "_" + date + ".gz";
