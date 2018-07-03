c214d0530e1c39f035864a024f07ad299757d90c
Dmitriy Dumanskiy <doom369@gmail.com>
2018-07-03 18:20:31 +0300

    #1161 build fix

--- server/core/src/main/java/cc/blynk/server/core/model/serialization/View.java
+++ server/core/src/main/java/cc/blynk/server/core/model/serialization/View.java
@@ -6,14 +6,17 @@ public class View {
      * special marker class that used to serialize all fields expect those one
      * that marked with Private annotation
      */
-    public static class PublicOnly {}
+    public static class PublicOnly {
+    }
 
     /**
      * special utility class that is used to mark private fields
      * that should not always be visible to the end users.
      */
-    public static class Private {}
+    public static class Private {
+    }
 
-    public static class HttpAPIField {}
+    public static class HttpAPIField {
+    }
 
 }
