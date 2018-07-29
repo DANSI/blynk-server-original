3d51ac3b44df21bd802da20925a3f6feb61fbb60
Dmitriy Dumanskiy <doom369@gmail.com>
2018-07-29 18:09:25 +0300

    #1006 moved to the new acme version

--- server/launcher/src/main/java/cc/blynk/server/workers/CertificateRenewalWorker.java
+++ server/launcher/src/main/java/cc/blynk/server/workers/CertificateRenewalWorker.java
@@ -4,9 +4,12 @@ import cc.blynk.server.SslContextHolder;
 import cc.blynk.server.acme.AcmeClient;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.shredzone.acme4j.util.CertificateUtils;
 
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
@@ -32,31 +35,41 @@ public class CertificateRenewalWorker implements Runnable {
         return TimeUnit.MILLISECONDS.toDays(expirationDate.getTime() - now);
     }
 
+    private static X509Certificate readX509Certificate() throws IOException {
+        try (InputStream fis = new FileInputStream(AcmeClient.DOMAIN_CHAIN_FILE)) {
+            CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
+            return (X509Certificate) certificateFactory.generateCertificate(fis);
+        } catch (CertificateException ex) {
+            throw new IOException(ex);
+        }
+    }
+
     @Override
     public void run() {
         try {
             if (AcmeClient.DOMAIN_CHAIN_FILE.exists()) {
                 //stream closed inside utilities method
-                X509Certificate cert = CertificateUtils.readX509Certificate(
-                        new FileInputStream(AcmeClient.DOMAIN_CHAIN_FILE));
+                X509Certificate cert = readX509Certificate();
 
                 Date expirationDate = cert.getNotAfter();
                 long daysToExpire = getDateDiff(expirationDate);
-                log.info("Certificate expiration date is {}. Days left : {}",
-                        expirationDate, daysToExpire);
+                log.info("Certificate expiration date is {}. Days left : {}", expirationDate, daysToExpire);
 
                 if (daysToExpire <= renewBeforeDays) {
-                    log.warn("Trying to renew...");
-                    sslContextHolder.regenerate();
-                    log.info("Success! The certificate for your domain has been renewed!");
+                    renew();
                 }
             } else {
-                sslContextHolder.regenerate();
-                log.info("Success! The certificate for your domain has been renewed!");
+                renew();
             }
         } catch (Exception e) {
             log.error("Error during certificate renewal.", e);
         }
+    }
+
+    private void renew() throws Exception {
+        log.warn("Trying to renew...");
+        sslContextHolder.regenerate();
+        log.info("Success! The certificate for your domain has been renewed!");
     }
 
 }
