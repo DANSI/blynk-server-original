98b727282ecdeac753d97153b478c526f5d6d748
Dmitriy Dumanskiy <doom369@gmail.com>
2018-11-03 17:14:59 +0200

    #1282 cleanup

--- server/core/src/main/java/cc/blynk/server/workers/timer/TimerWorker.java
+++ server/core/src/main/java/cc/blynk/server/workers/timer/TimerWorker.java
@@ -79,18 +79,19 @@ public class TimerWorker implements Runnable {
     private void init(ConcurrentMap<UserKey, User> users) {
         int counter = 0;
         for (Map.Entry<UserKey, User> entry : users.entrySet()) {
-            for (DashBoard dashBoard : entry.getValue().profile.dashBoards) {
-                for (Widget widget : dashBoard.widgets) {
+            for (DashBoard dash : entry.getValue().profile.dashBoards) {
+                int dashId = dash.id;
+                for (Widget widget : dash.widgets) {
                     if (widget instanceof DeviceTiles) {
                         DeviceTiles deviceTiles = (DeviceTiles) widget;
-                        counter += deviceTiles.addTimers(this, entry.getKey(), dashBoard.id);
+                        counter += deviceTiles.addTimers(this, entry.getKey(), dashId);
                     } else if (widget instanceof Timer) {
                         Timer timer = (Timer) widget;
-                        add(entry.getKey(), timer, dashBoard.id, -1, -1);
+                        add(entry.getKey(), timer, dashId, -1, -1);
                         counter++;
                     } else if (widget instanceof Eventor) {
                         Eventor eventor = (Eventor) widget;
-                        add(entry.getKey(), eventor, dashBoard.id);
+                        add(entry.getKey(), eventor, dashId);
                         counter++;
                     }
                 }
