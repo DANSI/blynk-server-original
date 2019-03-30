f09f5790e3a88a482933bdd4f7d1723604c184cd
Dmitriy Dumanskiy <doom369@gmail.com>
2019-03-30 09:18:12 +0200

    acme4j lib updated. module fix

--- server/acme/src/main/java/module-info.java
+++ server/acme/src/main/java/module-info.java
@@ -4,9 +4,9 @@
  * Created on 05.10.17.
  */
 module cc.blynk.server.acme {
-    requires acme4j.client;
     requires org.apache.logging.log4j;
-    requires acme4j.utils;
+    requires org.shredzone.acme4j.utils;
+    requires org.shredzone.acme4j;
 
     exports cc.blynk.server.acme;
 }
