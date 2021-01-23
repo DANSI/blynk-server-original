fc08022beaabb153860501bcccb999168af533fb
Dmitriy Dumanskiy <doom369@gmail.com>
2021-01-23 19:09:54 +0200

    lesser pool size

--- server/core/src/main/java/cc/blynk/server/db/DBManager.java
+++ server/core/src/main/java/cc/blynk/server/db/DBManager.java
@@ -108,7 +108,7 @@ public class DBManager implements Closeable {
 
         config.setAutoCommit(false);
         config.setConnectionTimeout(serverProperties.getLongProperty("connection.timeout.millis"));
-        config.setMaximumPoolSize(10);
+        config.setMaximumPoolSize(5);
         config.setMaxLifetime(0);
         config.setConnectionTestQuery("SELECT 1");
         return config;
